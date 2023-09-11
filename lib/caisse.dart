906c273d27e75dac09d97e1c8db934ce2278e7b1
NadjetBoumezoued <n.boumezoued@esi-sba.dz>
2023-09-11 01:31:18 +0100

    encaisser

---
+++ lib/caisse.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class Caisse extends StatefulWidget {
+  const Caisse({super.key});
+
+  @override
+  State<Caisse> createState() => _CaisseState();
+}
+
+class _CaisseState extends State<Caisse> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.check_circle,
+              size: 100,
+              color: Color(0xFFEC6294),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 30.0), // Marge de 20 à gauche et à droite
+              child: Text(
+                'Félicitations, vous avez complété une course !',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              'Montant à encaisser',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              '500 DA',
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFEC6294)),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              width: 300, // Largeur du conteneur
+              height: 200, // Hauteur du conteneur
+              decoration: BoxDecoration(
+                color: Color(0xFFFBEDF2), // Couleur rose
+                borderRadius: BorderRadius.circular(20.0), // Bordure arrondie
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Cout du voyage ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        '500 DA',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Frais Weslini  ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        '-50 DA ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Votre Gain    ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        '500 DA ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFFEC6294), // Background color
+                onPrimary: Colors.white, // Text color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0), // Border radius
+                ),
+              ),
+              onPressed: () {
+                // Your button's onPressed callback
+              },
+              child: Text(
+                'Encaisser',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
